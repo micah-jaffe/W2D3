6a68c4914b7dd9e59ad48896ab00a2d23df48771
micah-jaffe <micah.jaffe95@gmail.com>
2018-10-31 15:22:13 -0700

    Write specs for Game#initialize

--- tdd/Poker/spec/game_spec.rb
+++ tdd/Poker/spec/game_spec.rb
@@ -1,1 +1,24 @@
 require 'rspec'
+require 'game'
+
+RSpec.describe do
+  describe Game do
+    subject(:game) { Game.new }
+    
+    describe '#initialize' do
+      it 'receives a deck object' do
+        expect(game.deck).to be_a(Deck)
+      end
+      
+      it 'keeps track of whose turn it is' do
+        expect(game.turn).to be_a(Player)
+      end
+      
+      it 'keeps track of the amount in the pot' do
+        expect(game.pot).to be_a(Integer)
+      end
+    end
+    
+    
+  end
+end
