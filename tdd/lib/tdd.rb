0e2670cc13588e078b522a6a72abf56bf85a8333
micah-jaffe <micah.jaffe95@gmail.com>
2018-10-31 13:49:32 -0700

    Write Array#two_sum methods

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -1,3 +1,15 @@
 def my_uniq(arr)
   arr.uniq
 end
+
+class Array
+  def two_sum
+    res = []
+    (0..self.length - 2).each do |i|
+      ((i + 1)..(self.length - 1)).each do |j|
+        res << [i, j] if self[i] + self[j] == 0
+      end
+    end
+    res
+  end
+end
