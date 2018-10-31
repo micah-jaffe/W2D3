b34ef2beaa7ef8829b3b9c17c69011d0285f6ad2
micah-jaffe <micah.jaffe95@gmail.com>
2018-10-31 14:43:18 -0700

    Write TowersOfHanoi#initialize method and specs

--- tdd/spec/towers_of_hanoi_spec.rb
+++ tdd/spec/towers_of_hanoi_spec.rb
@@ -17,15 +17,17 @@ RSpec.describe do
     
     describe '#move' do
       it "moves a disk from one peg to another" do
-        
+        game.move(0, 1)
+        expect(game.board).to eq([[3,2], [1], []])
       end
       
       it "does not move a larger disk onto a smaller disk" do
-        
+        game.move(0, 1)
+        expect { game.move(0, 1) }.to raise_error("Invalid move")
       end
       
       it "does not allow you select an empty peg as start" do
-        
+        expect { game.move(1, 0) }.to raise_error("Empty peg")
       end
     end
     
