2bc18ef122c9ad7ff6db08269fa55894b565ceeb
micah-jaffe <micah.jaffe95@gmail.com>
2018-10-31 16:11:01 -0700

    Add Deck#initialize specs

---
+++ tdd/Poker/spec/deck_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require 'deck'
+
+RSpec.describe do 
+  describe Deck do
+    subject(:deck) { Deck.new }
+    describe "#initialize" do
+      it "creates a standard 52 card deck" do
+        expect(deck.cards.length).to eq(52)
+        expect(deck.cards.all? {|card| card.is_a?(Card)}).to be_true
+      end
+    end
+    
+    describe "::populate" do
+      
+    end
+    
+    describe "#shuffle" do
+      
+    end
+    
+    describe "#deal" do 
+      
+    end
+    
+  end
+end
