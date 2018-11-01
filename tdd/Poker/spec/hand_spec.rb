3bb2e4dd1de9a0f50c5e5f96735a887d656ddeaf
micah-jaffe <micah.jaffe95@gmail.com>
2018-10-31 17:31:38 -0700

    Add specs for Hand#initialize

--- tdd/Poker/spec/hand_spec.rb
+++ tdd/Poker/spec/hand_spec.rb
@@ -3,8 +3,15 @@ require 'hand'
 
 RSpec.describe do
   describe Hand do
+    subject(:hand) { Hand.new([1, 2, 3. 4, 5]) }
     describe '#initialize' do
+      it "receives an array as an argument" do
+        
+      end
       
+      it "receives card instances in its array" do
+        
+      end
     end
   end
 end
